1a998d6e62542465222a6c8db653cf351f7721da
luckydonald <mac2019.git@luckydonald.de>
2020-09-05 15:01:30 +0200

    [tg_file_id|sticker_tag old] Added a unittest for that.

---
+++ tests/test_file_unique_id.py
@@ -0,0 +1,17 @@
+import unittest
+
+from tg_file_id.file_id import FileId
+from tg_file_id.file_unique_id import FileUniqueId
+
+
+class MyTestCase(unittest.TestCase):
+    def test_sticker(self):
+        file_id_old = FileId.from_file_id('CAADAQADegAD997LEUiQZafDlhIeAg')
+        file_id_new = FileId.from_file_id('CAACAgEAAx0CVgtngQACAuFfU1GY9wiRG7A7jlIBbP2yvAostAACegAD997LEUiQZafDlhIeGwQ')
+        expected = 'AgADegAD997LEQ'
+        self.assertEqual(expected, FileUniqueId.from_file_id(file_id_old), 'Old style file ID.')
+        self.assertEqual(expected, FileUniqueId.from_file_id(file_id_new), 'Old style file ID.')
+
+
+if __name__ == '__main__':
+    unittest.main()
