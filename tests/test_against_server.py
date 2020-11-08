09307fd5a20191a590398d8fc1e3a3bae045748a
luckydonald <mac2019.git@luckydonald.de>
2020-11-08 22:00:46 +0100

    [file_id] Fixed server based tests.

--- tests/test_against_server.py
+++ tests/test_against_server.py
@@ -26,7 +26,10 @@ except ImportError:
 try:
     from somewhere import CHAT_ID
 except ImportError:
-    CHAT_ID = os.getenv('CHAT_ID', os.getenv('TG_CHAT_ID', None))  # -295187931: https://t.me/joinchat/AKOLAhGYNdvTEwPnp2vUYQ
+    CHAT_ID = os.getenv('CHAT_ID', os.getenv('TG_CHAT_ID', None))  # -1001377646352: https://t.me/joinchat/AKOLAhGYNdvTEwPnp2vUYQ
+# end if
+if CHAT_ID:
+    CHAT_ID = int(CHAT_ID)
 # end if
 
 
@@ -43,7 +46,7 @@ class MyTestCase(unittest.TestCase):
     # end def
 
     def test_send_sticker(self):
-        original_file_id = 'CAACAgIAAxkBAAIE5V-nWi9JU2kv35Mqw_QYkNwhTfIbAAK2BgACAoujAAFGAUWZ6DSbtR4E'
+        original_file_id = 'CAACAgIAAx0EUh03EAADDl-oWPT0MOIQZ93aktaAu8hU8rPEAAK2BgACAoujAAFGAUWZ6DSbtR4E'
         try:
             from pytgbot import Bot
         except ImportError:
@@ -60,6 +63,7 @@ class MyTestCase(unittest.TestCase):
         bot = Bot(API_KEY)
 
         msg = bot.send_sticker(chat_id=CHAT_ID, sticker=original_file_id)
+        print(msg)
 
         sticker_file = FileId.from_file_id(msg.sticker.file_id)
         sticker_file_unique_id = FileUniqueId.from_file_id(msg.sticker.file_id).to_unique_id()
@@ -72,7 +76,7 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(46033261910034102, sticker_file.id)
         self.assertEqual(-5360632757845950138, sticker_file.access_hash)
         self.assertEqual(2, sticker_file.dc_id)
-        self.assertEqual(b64decode('AQAABOVfp1ovSVNpL9+TKsP0GJDcIU3yGw=='), sticker_file.file_reference)
+        # self.assertEqual(b64decode('BFIdNxAAAAAOX6hY9PQw4hBn3dqS1oC7yFTys8Q='), sticker_file.file_reference)
 
         thumb_file = FileId.from_file_id(msg.sticker.thumb.file_id)
         # 'AAMCAgADGQMAAQHUiF-oKLkvxChbEROPTTw6Aagft9bPAAK2BgACAoujAAFGAUWZ6DSbtUufgioABAEAB20AAwpQAAIeBA'
@@ -86,12 +90,12 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(sticker_file.id, thumb_file.id)
         self.assertEqual(sticker_file.access_hash, thumb_file.access_hash)
         self.assertEqual(sticker_file.dc_id, thumb_file.dc_id)
-        self.assertEqual(sticker_file.file_reference, thumb_file.dc_id)
+        self.assertEqual(sticker_file.file_reference, thumb_file.file_reference)
         # PhotosizeSource
         self.assertIsInstance(thumb_file.photosize, PhotoFileId.PhotosizeSource)
         self.assertEqual(20490, thumb_file.photosize.location_local_id)
-        self.assertEqual(b'm', thumb_file.photosize.volume_id)
-        self.assertEqual(PhotoFileId.PHOTOSIZE_SOURCE_LEGACY, thumb_file.photosize.type_id)
+        self.assertEqual(713203531, thumb_file.photosize.volume_id)
+        self.assertEqual(PhotoFileId.PHOTOSIZE_SOURCE_THUMBNAIL, thumb_file.photosize.type_id)
         # PhotosizeSourceThumbnail
         self.assertIsInstance(thumb_file.photosize, PhotoFileId.PhotosizeSourceThumbnail)
         self.assertEqual(b'm', thumb_file.photosize.thumbnail_type)
@@ -115,13 +119,15 @@ class MyTestCase(unittest.TestCase):
         bot = Bot(API_KEY)
 
         chat = bot.get_chat(chat_id=CHAT_ID)
+        print(chat)
         if not chat.photo:
             return self.skipTest('Test chat has no picture')
         # end if
 
         # big picture
 
-        big_file = FileId.from_file_id(chat.photo.big_file_id)  # 'AQADAgATSd1hmi4AAwMAAyXKZ-7_____AAgligACHgQ'
+        big_file = FileId.from_file_id(chat.photo.big_file_id)  # 'AQADAgATqfDdly4AAwMAA4siCOX_____AAhKowIAAR4E'
+
         big_file_unique_id = FileUniqueId.from_file_id(chat.photo.big_file_id).to_unique_id()
         self.assertEqual(chat.photo.big_file_unique_id, big_file_unique_id)
 
@@ -136,8 +142,8 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(False, big_file.has_web_location)
 
         self.assertIsInstance(big_file.photosize, PhotoFileId.PhotosizeSource)
-        self.assertEqual(200158600521, big_file.photosize.volume_id)
-        self.assertEqual(35365, big_file.photosize.location_local_id)
+        # self.assertEqual(200116400297, big_file.photosize.volume_id)
+        # self.assertEqual(172874, big_file.photosize.location_local_id)
 
         self.assertEqual(PhotoFileId.PHOTOSIZE_SOURCE_DIALOGPHOTO_BIG, big_file.photosize.type_id)
         self.assertIsInstance(big_file.photosize, PhotoFileId.PhotosizeSourceDialogPhotoBig)
@@ -146,7 +152,7 @@ class MyTestCase(unittest.TestCase):
 
         # small picture
 
-        small_file = FileId.from_file_id(chat.photo.small_file_id)  # 'AQADAgATSd1hmi4AAwIAAyXKZ-7_____AAgjigACHgQ'
+        small_file = FileId.from_file_id(chat.photo.small_file_id)  # 'AQADAgATqfDdly4AAwIAA4siCOX_____AAhIowIAAR4E'
         small_file_unique_id = FileUniqueId.from_file_id(chat.photo.small_file_id).to_unique_id()
         self.assertEqual(chat.photo.small_file_unique_id, small_file_unique_id)
 
@@ -162,10 +168,10 @@ class MyTestCase(unittest.TestCase):
 
         self.assertIsInstance(small_file.photosize, PhotoFileId.PhotosizeSource)
         self.assertEqual(big_file.photosize.volume_id, small_file.photosize.volume_id)
-        self.assertEqual(big_file.photosize.location_local_id, small_file.photosize.location_local_id)
+        # self.assertEqual(172872, small_file.photosize.location_local_id)
 
-        self.assertEqual(PhotoFileId.PHOTOSIZE_SOURCE_DIALOGPHOTO_BIG, small_file.photosize.type_id)
-        self.assertIsInstance(small_file.photosize, PhotoFileId.PhotosizeSourceDialogPhotoBig)
+        self.assertEqual(PhotoFileId.PHOTOSIZE_SOURCE_DIALOGPHOTO_SMALL, small_file.photosize.type_id)
+        self.assertIsInstance(small_file.photosize, PhotoFileId.PhotosizeSourceDialogPhotoSmall)
         self.assertEqual(CHAT_ID, small_file.photosize.dialog_id)
         self.assertEqual(big_file.photosize.dialog_access_hash, small_file.photosize.dialog_access_hash)
     # end if
